31bd681008804da57388b82239d73a36638ee217
Josh Linton and Ryan Jones <josh@>
2017-03-20 10:34:56 -0700

    add the logic to dynamically set variables in the html

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,9 +11,29 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/CHANGEME.vtl");
+      model.put("template", "templates/home.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/form", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+
+    get("/story", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String person1 = request.queryParams("person1");
+      model.put("person1", person1);
+      model.put("person2", request.queryParams("person2"));
+      model.put("exclamation", request.queryParams("exclamation"));
+      model.put("noun", request.queryParams("noun"));
+      model.put("verb", request.queryParams("verb"));
+      model.put("animal", request.queryParams("animal"));
+      model.put("template", "templates/story.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
